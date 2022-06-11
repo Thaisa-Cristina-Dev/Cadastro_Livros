52cbc63fd4e010bf79d2bbd0ea287ca3689516c7
Thaisa-Cristina-Dev <94863117+Thaisa-Cristina-Dev@users.noreply.github.com>
2022-06-11 02:29:38 -0300

    Add files via upload

---
+++ Model/BookContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Model
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Book> Books { get; set; }
+        //É necessário criar um desse para cada entidade
+    }
+}
